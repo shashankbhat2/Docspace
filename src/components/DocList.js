3cd977a60dba17f125e72da05979d193f80809c5
Shashank Bhat <shashank.bhat2525@gmail.com>
2021-06-13 05:27:34 +0530

    initial commit

--- src/components/DocList.js
+++ src/components/DocList.js
@@ -1,31 +1,47 @@
-import React, { Fragment, useState } from 'react'
-import { Container, Alert,  CardDeck, Card, Form, Row,Col, Button } from 'react-bootstrap'
-
+import React, { Fragment, useEffect, useState } from 'react'
+import { Container, CardDeck, Card, Form, Row,Col, Button } from 'react-bootstrap'
+import {FaHeart} from 'react-icons/fa'
 const DocList = () => {
-    const [show, setShow] = useState(false);
     const [docs, setDocs] = useState([]);
-    const [newDoc, setnewDoc] = useState({link: '', name: ''})
-    const [toast, setToast] = useState({error: '', success: ''}) 
+    const [pinnedDocs, setPinnedDocs] = useState([]);
+    const [link, setLink] = useState('');
+    const [name, setName] = useState('');
+
+    useEffect(() => {
+        const existingDocs = JSON.parse(localStorage.getItem('storedDocs')) || [];
+        const existingPinnedDocs = JSON.parse(localStorage.getItem('pinnedDocs')) || [];
+        setDocs(existingDocs)
+        setPinnedDocs(existingPinnedDocs)
+    }, [])
 
     const handleAdd = () => {
-        if(validate()){
-            setToast({success: "Doc added successfully"})
             var doc = {
-                link: newDoc.link,
-                name: newDoc.name
+                link: link,
+                name: name,
+                pinned: false,
             }
             var newDocs = [doc, ...docs];
             setDocs(newDocs)
-        }
+            localStorage.setItem('storedDocs', JSON.stringify(newDocs))
     }
 
-    const validate = () => {
-        var isValid = true;
-        if(url === ""){
-            setToast({error: 'Please enter a valid url'});
-            isValid = false;
+    const handlePinning = (doc) => {
+        var pinnedDoc = {
+            ...doc,
+            pinned: true
         }
-        return isValid
+        console.log(pinnedDoc);
+        var newDocs = [pinnedDoc, ...pinnedDocs];
+        setPinnedDocs(newDocs)
+        localStorage.setItem('pinnedDocs', JSON.stringify(newDocs))
+    }
+
+    const handleRemove = (doc) =>{
+        console.log(doc)
+        var newDocs = [docs.splice(doc, 1), ...docs];
+        console.log(newDocs)
+        // localStorage.setItem('storedDocs', JSON.stringify(newDocs))
+        // setDocs(newDocs)
     }
 
     return(
@@ -37,14 +53,11 @@ const DocList = () => {
                         <h6 className="popup_heading">Add Your Documentation Link</h6>
                     </Col>
                     <Col md={6}>
-                         <Form.Control type="text" onChange={(e) => setnewDoc({name: e.target.value})} placeholder="Name" className="popup_input" />
-                         <Form.Control type="url" onChange={(e) => setnewDoc({link: e.target.value})} placeholder="Url" className="popup_input" />
+                         <Form.Control type="text" onChange={(e) => setName(e.target.value)} placeholder="Name" className="popup_input" />
+                         <Form.Control type="url" onChange={(e) => setLink(e.target.value)} placeholder="Url" className="popup_input" />
                          <Button variant="primary" onClick={handleAdd} className="add_button">
                              Add 
                          </Button>
-                         {/* <Toast onClose={() => setShow(false)} show={show} delay={3000} autohide>
-                            <Toast.Body>{toast.error ? toast.error : toast.success}</Toast.Body>
-                        </Toast> */}
                     </Col>
                     </Row>
                 </Container>
@@ -52,19 +65,57 @@ const DocList = () => {
                     <div className="pinned__base">
                         <h6 className="popup_heading">Your Pinned Docs</h6>
                         <CardDeck>
-                            {docs.map((doc, i) => (
+                            {pinnedDocs.length !== 0 ? docs.map((doc, i) => (
                                 <Card key={i}>
                                     <Card.Body>
                                         <Card.Title>
                                         {doc.name}
                                         </Card.Title>
-                                        <Button as="a" href={doc.link} variant="primary">Check</Button>
-                                        <Button variant="danger" onClick={handleAdd}>
+                                        <Button href={doc.link} className="doc_btn" variant="primary" target="_blank">Check</Button>
+                                        <Button variant="danger" onClick={() => handleRemove(doc)}>
                                             Remove
+                                        </Button>
+                                        <Button onClick={() => handlePinning(doc)}>
+                                            <FaHeart />
                                         </Button>
                                     </Card.Body>
                                 </Card>
-                            ))}
+                            )):(
+                                <div>
+                                    <Container>
+                                        <p className="docs_fallback">No Pinned Docs</p>
+                                    </Container>
+                                </div>
+                            )}
+                        </CardDeck>
+                    </div>
+                </Container>
+                <Container>
+                    <div className="pinned__base">
+                        <h6 className="popup_heading">Other Docs</h6>
+                        <CardDeck>
+                            {docs.length !== 0 ? docs.map((doc, i) => (
+                                <Card key={i}>
+                                    <Card.Body>
+                                        <Card.Title>
+                                        {doc.name}
+                                        </Card.Title>
+                                        <Button href={doc.link} className="doc_btn" variant="primary" target="_blank">Check</Button>
+                                        <Button className="doc_btn" variant="danger" onClick={() => handleRemove(doc)}>
+                                            Remove
+                                        </Button>
+                                        <Button  className="doc_btn" onClick={() => handlePinning(doc)}>
+                                            <FaHeart />
+                                        </Button>
+                                    </Card.Body>
+                                </Card>
+                            )):(
+                                <div>
+                                    <Container>
+                                        <p className="docs_fallback">No Docs</p>
+                                    </Container>
+                                </div>
+                            )}
                         </CardDeck>
                     </div>
                 </Container>
